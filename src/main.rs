5be32bcad728c3aa0352bac46de7936bb13604f2
M.Saeb <Mohammed.saeb98@hotmail.com>
2023-01-05 22:21:15 +0300

    CHANGE: create basic surrealdb sandbox

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use surrealdb::Datastore;
+use surrealdb::Error;
+use surrealdb::Session;
+
+async fn create_person(ds: &Datastore, session: &Session) -> Result<(), Error>{
+    let create_query = "USE NS test DB test; CREATE person SET
+        name = 'anything',
+        created_at = time::now()
+    ;";
+    let create_response = ds.execute(create_query, session, None, false).await?;
+    let create_result = &create_response[1].result;
+    println!("Create response {:?}", create_response);
+    Ok(())
+}
+
+async fn fetch_persons(ds: &Datastore, session: &Session) -> Result<(), Error>{
+    let fetch_query = "USE NS test DB test; SELECT * FROM person;";
+    let fetch_response = ds.execute(fetch_query, session, None, false).await?;
+    let fetch_response = &fetch_response[1].result;
+    println!("Fetch response {:?}", fetch_response);
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let ds = Datastore::new("file://database.db").await?;
+    let root_session = Session::for_kv();
+
+    // create_person(&ds, &root_session).await?;
+
+    fetch_persons(&ds, &root_session).await?;
+
+    Ok(())
 }
